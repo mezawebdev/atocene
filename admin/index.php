7f72e5ab078f86c9992c4e788af13ca86cd4e03f
Alex Meza <mezawebdev@gmail.com>
2017-07-21 05:37:03 -0700

    Added intro title

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Avocados</title>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge />">
+	<meta http-equiv="content-type" content="text/html" charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="author" content="Alex Meza" />
+	<meta name="keywords" content="HTML, CSS, JavaScript, jQuery, jQueryUI, Bootstrap" />
+	<meta name="prototype" content="0" />
+	<meta name="date" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" href="../assets/css/animate.css" />
+	<link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="../assets/css/main.css" />
+	<link rel="stylesheet" href="../assets/css/jquery-ui.min.css">
+</head>
+<body>
+	<!-- wrapper start -->
+		<canvas id="gradient-background">
+
+		</canvas>
+		<div class="container">
+			<h1>Welcome</h1>
+		</div>
+	<!-- wrapper ends -->
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/jquery-ui.min.js"></script>
+	<script src="../assets/js/bootstrap.min.js"></script>
+	<script src="../assets/js/granim.min.js"></script>
+	<script src="../assets/js/main.js"></script>
+</body>
+</html>
---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var granimInstance = new Granim({
+        element: '#gradient-background',
+        direction: 'top-bottom',
+        opacity: [1, .5, 0],
+        isPausedWhenNotInView: true,
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#485563', '#29323c', '#601B24'],
+                    ['#00c6ff', '#002A60', '#0050B5']
+                ],
+                transitionSpeed: 10000
+            }
+        }
+    });
+});
